d4c1c468f6d49fb5c1dfa41462901552a40c07eb
David Fry <david.fry.tv@gmail.com>
2015-09-02 14:39:25 -0700

    Method error fixed

--- client/templates/slidefade/slidefade.js
+++ client/templates/slidefade/slidefade.js
@@ -14,31 +14,23 @@ Template.slidefade.onRendered(function() {
     var currentSlide = 0;
     var slides = [1,2,3,4];
     var length = slides.length;
-    var firstLoad = 0;
 
-    console.log(length);
-
-    Meteor.call('test', function () {
-       console.log(slides);
-    });
+    var slideConfig = {
+        slideDuration: 3000,
+        transitionDuration: 1000,
+        slideArray: [1,2,3,4],
+        selector: '.slide'
+    };
 
     setInterval(function() {
         if (currentSlide >= length)
         {
             currentSlide = 0;
-            //fadeDownNew(slides);
-
             // This call is in the element Fade.js file
-            Meteor.call('fadeOutLastElement', slides, '.slide', slides[currentSlide], 0, 1000 )
-
+            fadeOutLastElement(slides, '.slide', slides[currentSlide], 0, 1000);
         }
-
-        console.log(currentSlide);
-        console.log(slides[currentSlide]);
-        //fadeUp(slides[currentSlide]);
-
         // This call is in the element Fade.js file
-        Meteor.call('fadeInOut', '.slide', slides[currentSlide], 1, 1000);
+        fadeInOut('.slide', slides[currentSlide], 1, 1000);
         currentSlide++;
     }, 3000);
 
@@ -49,36 +41,5 @@ Template.slidefade.onDestroyed(function () {
     //add your statement here
 });
 
-
-// Helper function
-function fadeUp(slideNum)
-{
-    $(".slide" + slideNum).animate({opacity: 1}, 1000);
-    //$(".slide" + slideNum).fadeIn(1000);
-    console.log("fade up " + slideNum);
-    return true
-}
-
-function fadeDown(slideNum)
-{
-    $(".slide" + slideNum).animate({opacity: 0}, 1000);
-    //$(".slide" + slideNum).fadeOut(1000);
-    console.log("fade down " + slideNum);
-    return true
-}
-function fadeDownNew(array)
-{
-
-    for (var index = 0; index < array.length; index++ )
-    {
-        //
-        $(".slide" + index).css('opacity', '0');
-
-    }
-    // fades the last photo in the array
-    fadeDown(array[array.length - 1]);
-
-
-}
 
 
