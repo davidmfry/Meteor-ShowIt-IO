7bc4771e50cf2dcbb2eab14fe647cc40b37f3d79
David Fry <david.fry.tv@gmail.com>
2015-09-10 08:57:52 -0700

    Dynamic Slides are working

--- client/templates/users/slideshows/slideshow.js
+++ client/templates/users/slideshows/slideshow.js
@@ -3,17 +3,24 @@ Template.slideshow.helpers({
     images: function () {
         return Images.find({'slideshowId': this.slideshowImagesId});
     },
-    slideshowTitle: function () {
-        return this.title;
+    title: function () {
+        return Session.get('slideshowTitle');
     },
     username: function () {
         return Meteor.user().profile.first_name;
+    },
+    test: {
+        images: function() {
+            return Images.find({'slideshowId': this.slideshowImagesId});
+        },
+        title: this.title
     }
 });
 
 Template.slideshow.onRendered(function () {
     // The current slideshow record in the mongo db
     var currentSlideshow = SlideShows.findOne({_id:Template.parentData(0)._id});
+    Session.set('slideshowTitle', currentSlideshow.title);
     var currentSlide = 0;
     var selector = '.slide';
     console.log(currentSlideshow.seqArray);
